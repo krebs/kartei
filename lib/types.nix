8257771a429d327c0f5626c85416c987a011b417
Jörg Thalheim <joerg@thalheim.io>
2026-07-17 11:18:03 +0200

    lib/types: remove unused types
    flameshot.color, svg.color-keyword, secret-file, source, group,
    haskell.*, systemd.unit-name and friends have no consumers in kartei;
    they only served stockholm, which carries its own copies in
    lib/types.nix. Drop them along with svg-colors.json and the now-unused
    lib inherits.

--- lib/types.nix
+++ lib/types.nix
@@ -1,12 +1,11 @@
 { lib, ... }:
 let
   inherit (lib)
-    all any attrNames concatMapStringsSep concatStringsSep const filter flip
-    genid_uint31 hasSuffix head importJSON isInt isString length mergeOneOption
-    mkOption mkOptionType optional optionalAttrs optionals range splitString
-    stringLength substring test testString typeOf;
+    all concatMapStringsSep concatStringsSep const filter genid_uint31 isInt
+    isString length mergeOneOption mkOption mkOptionType optional optionals
+    range splitString stringLength substring test testString typeOf;
   inherit (lib.types)
-    addCheck attrsOf bool either enum int lines listOf nullOr path str submodule;
+    attrsOf bool either enum int lines listOf nullOr path str submodule;
 in
 
 rec {
@@ -311,21 +310,9 @@ rec {
     };
   });
 
-  boundedInt = min: max: mkOptionType {
-    name = "bounded integer";
-    check = x: isInt x && min <= x && x <= max;
-    merge = mergeOneOption;
-  };
-
   lowerBoundedInt = min: mkOptionType {
     name = "lower bounded integer";
     check = x: isInt x && min <= x;
-    merge = mergeOneOption;
-  };
-
-  positive = mkOptionType {
-    inherit (lowerBoundedInt 1) check;
-    name = "positive integer";
     merge = mergeOneOption;
   };
 
@@ -334,140 +321,6 @@ rec {
     name = "unsigned integer";
     merge = mergeOneOption;
   };
-
-  secret-file = submodule ({ config, ... }: {
-    options = {
-      name = mkOption {
-        type = pathname;
-        default = config._module.args.name;
-      };
-      path = mkOption {
-        type = absolute-pathname;
-        default = "/run/keys/${config.name}";
-        defaultText = "/run/keys/‹name›";
-      };
-      mode = mkOption {
-        type = file-mode;
-        default = "0400";
-      };
-      owner = mkOption {
-        type = user;
-      };
-      group-name = mkOption {
-        type = str;
-        default = "root";
-      };
-      service = mkOption {
-        type = systemd.unit-name;
-        default = "secret-${lib.systemd.encodeName config.name}.service";
-        defaultText = "secret-‹name›.service";
-      };
-      source-path = mkOption {
-        type = str;
-        default = config.name;
-        defaultText = "‹secrets/‹name››";
-      };
-    };
-  });
-
-
-  source = submodule ({ config, ... }: {
-    options = {
-      type = let
-        known-types = attrNames source-types;
-        type-candidates = filter (k: config.${k} != null) known-types;
-      in mkOption {
-        default = if length type-candidates == 1
-                    then head type-candidates
-                    else throw "cannot determine type";
-        type = enum known-types;
-      };
-      file = mkOption {
-        apply = x:
-          if absolute-pathname.check x
-            then { path = x; }
-            else x;
-        default = null;
-        type = nullOr (either absolute-pathname source-types.file);
-      };
-      git = mkOption {
-        default = null;
-        type = nullOr source-types.git;
-      };
-      pass = mkOption {
-        default = null;
-        type = nullOr source-types.pass;
-      };
-      pipe = mkOption {
-        apply = x:
-          if absolute-pathname.check x
-            then { command = x; }
-            else x;
-        default = null;
-        type = nullOr (either absolute-pathname source-types.pipe);
-      };
-      symlink = mkOption {
-        type = nullOr (either pathname source-types.symlink);
-        default = null;
-        apply = x:
-          if pathname.check x
-            then { target = x; }
-            else x;
-      };
-    };
-  });
-
-  source-types = {
-    file = submodule {
-      options = {
-        path = mkOption {
-          type = absolute-pathname;
-        };
-      };
-    };
-    git = submodule {
-      options = {
-        ref = mkOption {
-          type = str; # TODO types.git.ref
-        };
-        url = mkOption {
-          type = str; # TODO types.git.url
-        };
-      };
-    };
-    pass = submodule {
-      options = {
-        dir = mkOption {
-          type = absolute-pathname;
-        };
-        name = mkOption {
-          type = pathname; # TODO relative-pathname
-        };
-      };
-    };
-    pipe = submodule {
-      options = {
-        command = mkOption {
-          type = absolute-pathname;
-        };
-      };
-    };
-    symlink = submodule {
-      options = {
-        target = mkOption {
-          type = pathname; # TODO relative-pathname
-        };
-      };
-    };
-
-  };
-
-  suffixed-str = suffs:
-    mkOptionType {
-      name = "string suffixed by ${concatStringsSep ", " suffs}";
-      check = x: isString x && any (flip hasSuffix x) suffs;
-      merge = mergeOneOption;
-    };
 
   user = submodule ({ config, ... }: {
     options = {
@@ -522,21 +375,6 @@ rec {
       };
     };
   });
-  group = submodule ({ config, ... }: {
-    options = {
-      name = mkOption {
-        type = username;
-        default = config._module.args.name;
-        defaultText = "genid_uint31 ‹name›";
-      };
-      gid = mkOption {
-        type = int;
-        default = genid_uint31 config.name;
-        defaultText = "genid_uint31 ‹name›";
-      };
-    };
-  });
-
   addr = either addr4 addr6;
   addr4 = mkOptionType {
     name = "IPv4 address";
@@ -618,65 +456,6 @@ rec {
 
   tinc-pubkey = str;
 
-  krebs.file-location = submodule {
-    options = {
-      # TODO user
-      host = mkOption {
-        type = host;
-      };
-      # TODO merge with ssl.privkey.path
-      path = mkOption {
-        type = either path str;
-        apply = x: {
-          path = toString x;
-          string = x;
-        }.${typeOf x};
-      };
-    };
-  };
-
-  flameshot.color =
-    either (addCheck str (test "#[0-9A-Fa-f]{6}")) svg.color-keyword;
-
-  file-mode = mkOptionType {
-    name = "file mode";
-    check = test "[0-7]{4}";
-    merge = mergeOneOption;
-  };
-
-  haskell.conid = mkOptionType {
-    name = "Haskell constructor identifier";
-    check = test "[[:upper:]][[:lower:]_[:upper:]0-9']*";
-    merge = mergeOneOption;
-  };
-
-  haskell.modid = mkOptionType {
-    name = "Haskell module identifier";
-    check = x: isString x && all haskell.conid.check (splitString "." x);
-    merge = mergeOneOption;
-  };
-
-  # SVG 1.1, 4.4 Recognized color keyword names
-  #
-  # svg-colors.json has been generated with:
-  #   curl -sS https://www.w3.org/TR/SVG11/types.html#ColorKeywords |
-  #   fq -d html '[
-  #     grep_by(.["@class"]=="color-keywords") |
-  #     grep_by(.["@class"]=="prop-value"and.["#text"]!="").["#text"]
-  #   ] | sort'
-  #
-  svg.color-keyword = enum (importJSON ./svg-colors.json) // {
-    name = "SVG 1.1 recognized color keyword";
-  };
-
-  systemd.unit-name = mkOptionType {
-    name = "systemd unit name";
-    check = x:
-      test "^[0-9A-Za-z:_.\\-]+@?\\.(service|socket|device|mount|automount|swap|target|path|timer|slice|scope)$" x &&
-      stringLength x <= 256;
-    merge = mergeOneOption;
-  };
-
   # RFC952, B. Lexical grammar, <hname>
   hostname = mkOptionType {
     name = "hostname";
@@ -690,13 +469,6 @@ rec {
     name = "label";
     # TODO case-insensitive labels
     check = test "[0-9A-Za-z]([0-9A-Za-z-]*[0-9A-Za-z])?";
-    merge = mergeOneOption;
-  };
-
-  # POSIX.1‐2017, 3.190 Group Name
-  groupname = mkOptionType {
-    name = "POSIX group name";
-    check = filename.check;
     merge = mergeOneOption;
   };
 
